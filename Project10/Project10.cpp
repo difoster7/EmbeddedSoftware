4fbf3b14b273c36b3413f59f11563f871b55b3df
David Foster <davidianfoster@gmail.com>
2025-05-02 23:04:05 -0400

    Testing float reads

--- Project10/Project10.cpp
+++ Project10/Project10.cpp
@@ -2,6 +2,59 @@
 #include <wiringPiSPI.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdint.h>
+
+#define SPI_CHANNEL 0
+#define SPI_SPEED 500000
+
+int main(void) {
+    if (wiringPiSetup() == -1) return 1;
+    if (wiringPiSPISetup(SPI_CHANNEL, SPI_SPEED) == -1) return 1;
+
+    unsigned char byte;
+    unsigned char floatBytes[12]; // 3 floats
+
+    while (1) {
+        // Find start-of-frame marker (0xAA)
+        do {
+            byte = 0x00;
+            wiringPiSPIDataRW(SPI_CHANNEL, &byte, 1);
+        } while (byte != 0xAA);
+
+        // Read next 12 bytes (3 floats)
+        for (int i = 0; i < 12; i++) {
+            floatBytes[i] = 0x00;
+            wiringPiSPIDataRW(SPI_CHANNEL, &floatBytes[i], 1);
+        }
+
+        // Reconstruct floats using a union
+        union {
+            unsigned char b[4];
+            float f;
+        } u;
+
+        printf("Received floats: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                u.b[j] = floatBytes[i * 4 + j];
+            }
+            printf("%.5f ", u.f);
+        }
+        printf("\n");
+
+        sleep(1);
+    }
+
+    return 0;
+}
+
+
+
+/*
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #define SPI_CHANNEL 0
 #define SPI_SPEED 500000
@@ -49,3 +102,4 @@ int main(void)
         sleep(1);
     }
 }
+    */
--- Project10/SPI_Test/SPI_Test.ino
+++ Project10/SPI_Test/SPI_Test.ino
@@ -1,7 +1,58 @@
 #include <SPI.h>
 
-volatile byte dataToSend = 0x42; // Example byte
+volatile byte sendBuffer[13]; // 1 marker + 3 floats * 4 bytes
+volatile byte byteIndex = 0;
+
+void setup() {
+  pinMode(MISO, OUTPUT);
+  SPCR |= _BV(SPE);  // Enable SPI
+  SPCR |= _BV(SPIE); // Enable SPI interrupt
+  sei();
+  prepareData();
+}
+
+void prepareData() {
+  union {
+    float f;
+    byte b[4];
+  } data;
+
+  // Example floats
+  float values[3] = {3.14, 2.718, 1.618};
+
+  sendBuffer[0] = 0xAA; // Start-of-frame marker
+
+  for (int i = 0; i < 3; i++) {
+    data.f = values[i];
+    for (int j = 0; j < 4; j++) {
+      sendBuffer[1 + i * 4 + j] = data.b[j];
+    }
+  }
+}
+
+ISR(SPI_STC_vect) {
+  byte dummy = SPDR;
+  SPDR = sendBuffer[byteIndex];
+  byteIndex = (byteIndex + 1) % 13;
+}
+
+void loop() {
+  // Optionally update values and call prepareData() again
+  delay(1000);
+}
+
+
+/*
+#include <SPI.h>
+
+union{
+  float f;
+  byte b[4];
+} data;
+
+volatile int byteToSend;
 volatile boolean dataSent = false;
+volatile byte sendBuffer[5];
 
 void setup() {
   Serial.begin(9600);
@@ -9,19 +60,37 @@ void setup() {
   SPCR |= _BV(SPE);  // Enable SPI
   SPCR |= _BV(SPIE); // Enable SPI interrupt
   sei();             // Enable global interrupts
+  data.f = 1.234;
+  byteToSend = 0;
+  sendBuffer[0] = 0xAA; // Start marker
+  for (int i = 0; i < 4; i++) {
+    sendBuffer[i + 1] = data.b[i];
+  }
 }
 
 ISR(SPI_STC_vect) {
   Serial.println("Received SPI Interrupt");
-  byte received = SPDR;  // Read byte from master
-  SPDR = dataToSend;     // Load data to send next
-  dataSent = true;
+  if (byteToSend == 0)
+  {
+    Serial.println("~~~~~~~~~~~~~");
+  }
+  SPDR = sendBuffer[byteToSend];
+  Serial.print("Sending Byte: ");
+  Serial.println(byteToSend);
+  Serial.print("Sending ");
+  Serial.println(sendBuffer[byteToSend], HEX);
+  byteToSend = (byteToSend + 1) % 5;
 }
 
 void loop() {
+  /*
   if (dataSent) {
     // Optional: Change the byte to send next time
-    dataToSend++;
+    data.f++;
+    Serial.print("data is now ");
+    Serial.println(data.f);
+    Serial.println(data.b[3]);
     dataSent = false;
   }
 }
+*/
